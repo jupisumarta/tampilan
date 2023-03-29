762c9974480ca3c0dff99ba8838dfdca96149384
jupisumarta <joops089663@gmail.com>
2023-03-29 10:39:41 +0700

    ajkajak

---
+++ SISTAMET/js/news-slide.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("#news-slider11").owlCarousel({
+        items : 5,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        pagination:true,
+        autoPlay:true
+    });
+});
+
+$(document).ready(function() {
+    $("#news-slider12").owlCarousel({
+        items : 5,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        pagination:true,
+        autoPlay:true
+    });
+});
